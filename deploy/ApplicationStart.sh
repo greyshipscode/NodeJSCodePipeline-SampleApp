7946c3b39492db027f5a60e514be3a56d4baf47b
Grey Newell <greyshipscode@gmail.com>
2020-12-01 20:38:25 -0500

    + fix start and validate service

--- deploy/ApplicationStart.sh
+++ deploy/ApplicationStart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Starting Application..."
-nohup ./node-app &
+./node-app &
--- deploy/ValidateService.sh
+++ deploy/ValidateService.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "Validating Application..."
-sleep 5
-curl -i 127.0.0.1:80/status
+curl -i localhost:80/status
