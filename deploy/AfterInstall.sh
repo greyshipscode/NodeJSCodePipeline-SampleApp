a78c0290061a12612369a9ffb8dfd5297b825974
Grey Newell <greyshipscode@gmail.com>
2020-12-01 21:14:15 -0500

    + use systemctl to wrap node process as service

--- deploy/AfterInstall.sh
+++ deploy/AfterInstall.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo "Performing cleanup after install..."
+
+echo "[Unit]
+Description=ROT13 demo service
+After=network.target
+StartLimitIntervalSec=0
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=ec2-user
+ExecStart=/srv/nodejs/node-app
+
+[Install]
+WantedBy=multi-user.target" >> /etc/systemd/system/node-app.service
---
+++ deploy/ApplicationStart.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+systemctl start node-app
+systemctl enable node-app
